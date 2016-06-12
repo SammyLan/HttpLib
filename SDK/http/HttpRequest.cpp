7c2e75226a6a76541cc17809af47f6ebe94cbc8c
SammyLan <sammylan234@gmail.com>
2016-06-13 01:28:24 +0800

    log

--- SDK/http/HttpRequest.cpp
+++ SDK/http/HttpRequest.cpp
@@ -4,6 +4,21 @@
 #include <iostream>
 #include <fstream>
 
+std::string CHttpRequest::strProxyHost;
+std::string CHttpRequest::strProxyUsrPwd;
+
+TCHAR LOGCURLDEBUG[] = _T("CURLDEBUG");
+void CHttpRequest::setProxy(std::string const & strIP, std::string const & strPort, std::string const &strUserName, std::string const & strPassword)
+{
+	if (!strIP.empty())
+	{
+		strProxyHost = strIP + ":" + strPort;
+		if (!strUserName.empty())
+		{
+			strProxyUsrPwd = strUserName + ":" + strPassword;
+		}
+	}
+}
 
 CHttpRequest::CHttpRequest(CHttpSession *pSession)
 	:pSession_(pSession)
@@ -14,14 +29,19 @@ CHttpRequest::CHttpRequest(CHttpSession *pSession)
 		LogFinal(HTTPLOG,_T("\ncurl_easy_init() failed, exiting!"));
 		exit(2);
 	}
+#ifdef DEBUG
+	curl_easy_setopt(this->handle_, CURLOPT_VERBOSE, 1L);
+	curl_easy_setopt(this->handle_, CURLOPT_DEBUGFUNCTION, &CHttpRequest::debug_callback);
+	curl_easy_setopt(this->handle_, CURLOPT_DEBUGDATA, this);
+#endif
 	curl_easy_setopt(this->handle_, CURLOPT_WRITEFUNCTION, write_cb);
 	curl_easy_setopt(this->handle_, CURLOPT_WRITEDATA, this);
-	curl_easy_setopt(this->handle_, CURLOPT_VERBOSE, 1L);
+	
 	curl_easy_setopt(this->handle_, CURLOPT_ERRORBUFFER, this->error);
 	curl_easy_setopt(this->handle_, CURLOPT_PRIVATE, this);
-	curl_easy_setopt(this->handle_, CURLOPT_NOPROGRESS, 1L);
-	curl_easy_setopt(this->handle_, CURLOPT_PROGRESSFUNCTION, prog_cb);
-	curl_easy_setopt(this->handle_, CURLOPT_PROGRESSDATA, this);
+	//curl_easy_setopt(this->handle_, CURLOPT_NOPROGRESS, 1L);
+	//curl_easy_setopt(this->handle_, CURLOPT_PROGRESSFUNCTION, prog_cb);
+	//curl_easy_setopt(this->handle_, CURLOPT_PROGRESSDATA, this);
 	curl_easy_setopt(this->handle_, CURLOPT_LOW_SPEED_TIME, 3L);
 	curl_easy_setopt(this->handle_, CURLOPT_LOW_SPEED_LIMIT, 10L);
 
@@ -37,6 +57,15 @@ CHttpRequest::CHttpRequest(CHttpSession *pSession)
 	curl_easy_setopt(this->handle_, CURLOPT_TCP_KEEPIDLE, 120L);
 	curl_easy_setopt(this->handle_, CURLOPT_TCP_KEEPINTVL, 60L);
 	curl_easy_setopt(this->handle_, CURLOPT_NOSIGNAL, 1);
+	if (!strProxyHost.empty())
+	{
+		curl_easy_setopt(handle_, CURLOPT_PROXY, strProxyHost.c_str());
+		if (!strProxyUsrPwd.empty())
+		{
+			curl_easy_setopt(handle_, CURLOPT_PROXYUSERPWD, strProxyUsrPwd.c_str());
+		}
+	}
+	
 }
 
 CHttpRequest::~CHttpRequest()
@@ -135,7 +164,7 @@ size_t CHttpRequest::write_cb(void *ptr, size_t size, size_t nmemb, CHttpRequest
 }
 
 /* CURLOPT_PROGRESSFUNCTION */
-int CHttpRequest::prog_cb(CHttpRequest *pThis, double dltotal, double dlnow, double ult, double uln)
+/*int CHttpRequest::prog_cb(CHttpRequest *pThis, double dltotal, double dlnow, double ult, double uln)
 {
 	(void)ult;
 	(void)uln;
@@ -143,6 +172,37 @@ int CHttpRequest::prog_cb(CHttpRequest *pThis, double dltotal, double dlnow, dou
 	LogFinal(HTTPLOG,_T("\nProgress: %S (%g/%g)"), pThis->url_.c_str(), dlnow, dltotal);
 	LogFinal(HTTPLOG,_T("\nProgress: %S (%g)"), pThis->url_.c_str(), ult);
 
+	return 0;
+}*/
+
+int CHttpRequest::debug_callback(CURL *handle, curl_infotype type, char *data, size_t size, CHttpRequest * pThis)
+{
+	const char *text;
+	(void)handle; /* prevent compiler warning */
+
+	switch (type){
+	case CURLINFO_TEXT:
+		LogFinal(LOGCURLDEBUG, _T("%p:== Info:\r\n%S"), handle,data);
+		break;	
+	case CURLINFO_HEADER_IN:
+		LogFinal(LOGCURLDEBUG, _T("%p:<= Recv header:\r\n%S"), handle,data);
+		break;
+	case CURLINFO_HEADER_OUT:
+		LogFinal(LOGCURLDEBUG, _T("%p:=> Send header:\r\n%S"), handle,data);
+		break;
+	case CURLINFO_DATA_OUT:
+		text = "=> Send data";
+		break;
+	case CURLINFO_SSL_DATA_OUT:
+		text = "=> Send SSL data";
+		break;
+	case CURLINFO_DATA_IN:
+		text = "<= Recv data";
+		break;
+	case CURLINFO_SSL_DATA_IN:
+		text = "<= Recv SSL data";
+		break;
+	}
 	return 0;
 }
 
--- SDK/http/HttpRequest.h
+++ SDK/http/HttpRequest.h
@@ -6,8 +6,11 @@ class CHttpSession;
 class CHttpRequest
 {
 public:
+	static void setProxy(std::string const & strIP, std::string const & strPort, std::string const &strUserName, std::string const & strPassword);
+
 	CHttpRequest(CHttpSession *pSession);
 	~CHttpRequest();
+
 	void setUrl(const std::string&url, const http::Parameters & para);	
 	void setHeader(const http::Header & header);
 	void setFormContent(const std::vector<std::pair<std::string, std::string>> & vecName2Content);
@@ -22,13 +25,16 @@ public:
 	void onDone(CURLcode res);	
 private:
 	static size_t write_cb(void *ptr, size_t size, size_t nmemb, CHttpRequest *pThis);
-	static int prog_cb(CHttpRequest *pThis, double dltotal, double dlnow, double ult, double uln);
+	//static int prog_cb(CHttpRequest *pThis, double dltotal, double dlnow, double ult, double uln);
 	static curl_socket_t opensocket(CHttpSession *pThis, curlsocktype purpose, struct curl_sockaddr *address);
 	static int close_socket(CHttpSession *pThis, curl_socket_t item);
+	static int debug_callback(CURL *handle,curl_infotype type,	char *data,	size_t size, CHttpRequest * pThis);
 public:
 	CURL *	handle_;
 	std::string	url_;
 	CHttpSession *	pSession_;
 	char error[CURL_ERROR_SIZE];
+	static std::string strProxyHost;
+	static std::string strProxyUsrPwd;
 };
 
